1607b71379b5c12566d7b21ade6a9d285ed18ec0
ncarabin <noe.carabin@epitech.eu>
2025-06-13 17:25:33 +0200

    Add: Two tests scripts

---
+++ server/script/flunch.sh
@@ -0,0 +1,8 @@
+##
+## EPITECH PROJECT, 2025
+## B-YEP-400-STG-4-1-zappy-noe.carabin
+## File description:
+## flunch
+##
+
+./zappy_server -p 4241 -x 20 -y 20 -n blue red green -c 4 -f 100
---
+++ server/script/test_connexion.sh
@@ -0,0 +1,8 @@
+##
+## EPITECH PROJECT, 2025
+## B-YEP-400-STG-4-1-zappy-noe.carabin
+## File description:
+## test_connexion
+##
+
+nc localhost 4241
