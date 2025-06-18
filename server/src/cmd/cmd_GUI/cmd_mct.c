178cbc55ff16fe6d2ca809c8a267cd2053c6499f
ncarabin <noe.carabin@epitech.eu>
2025-06-18 23:21:27 +0200

    Fix: Remove comment, and decoment the good func

--- server/src/cmd/cmd_GUI/cmd_mct.c
+++ server/src/cmd/cmd_GUI/cmd_mct.c
@@ -9,35 +9,10 @@
 #include "include/function.h"
 #include "include/structure.h"
 
-
-//ICI LA VRAI COMMANDE MCT JUSTE SUPPR CELLE DU DESSOUS ET DECOM
-//CELLE Là ET FONCTION IMPLEMENTER
-// void cmd_mct(server_t *server, int index, char **args)
-// {
-//     int fd;
-//     const resources_t *tile;
-
-//     (void)args;
-//     if (!server || !server->poll.client_list || index < 0 ||
-//         index >= server->poll.client_index)
-//         return;
-//     if (check_graphical(server, index) != 0)
-//         return;
-//     fd = server->poll.pollfds[index].fd;
-//     for (unsigned y = 0; y < server->height; y++) {
-//         for (unsigned x = 0; x < server->width; x++) {
-//             tile = &server->map[y][x];
-//             dprintf(fd, "bct %u %u %u %u %u %u %u %u %u\n",
-//                 x, y, tile->food, tile->linemate, tile->deraumere,
-//                 tile->sibur, tile->mendiane, tile->phiras, tile->thystame);
-//         }
-//     }
-// }
-
-//CI DESSOUS LA VERSION DE TEST SANS LA MAP
 void cmd_mct(server_t *server, int index, char **args)
 {
     int fd;
+    const resources_t *tile;
 
     (void)args;
     if (!server || !server->poll.client_list || index < 0 ||
@@ -48,7 +23,10 @@ void cmd_mct(server_t *server, int index, char **args)
     fd = server->poll.pollfds[index].fd;
     for (unsigned y = 0; y < server->height; y++) {
         for (unsigned x = 0; x < server->width; x++) {
-            dprintf(fd, "bct %u %u 0 0 0 0 0 0 0\n", x, y);
+            tile = &server->map[y][x];
+            dprintf(fd, "bct %u %u %u %u %u %u %u %u %u\n",
+                x, y, tile->food, tile->linemate, tile->deraumere,
+                tile->sibur, tile->mendiane, tile->phiras, tile->thystame);
         }
     }
 }
