9622c23df69459164d78c2d1a4e19ab0ac94c728
ncarabin <noe.carabin@epitech.eu>
2025-06-18 23:44:54 +0200

    Fix: Change the pos type on the fork cmd

--- server/src/cmd/cmd_IA/cmd_fork.c
+++ server/src/cmd/cmd_IA/cmd_fork.c
@@ -20,6 +20,6 @@ void cmd_fork(server_t *server, int index, char **args)
     if (check_autorized(server, index) != 0)
         return;
     pl = server->poll.client_list[index].player;
-    add_eggs(server, pl->id, pl->team, pl->position);
+    add_eggs(server, pl->id, pl->team, (int *)pl->position);
     dprintf(pl->socket_fd, "ok\n");
 }
