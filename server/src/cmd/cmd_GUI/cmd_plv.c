74c7b43435ff2ba1f8cd5a26ef398ed86f594953
ncarabin <noe.carabin@epitech.eu>
2025-06-18 23:33:36 +0200

    Refacto: Due to last change, the func to locate player by id is now a global func. We delete the local copy

--- server/src/cmd/cmd_GUI/cmd_plv.c
+++ server/src/cmd/cmd_GUI/cmd_plv.c
@@ -32,45 +32,6 @@ static int parse_plv_args(char **args, unsigned int *id_out)
     return 0;
 }
 
-/**
- * @brief Searches for a player with the given ID in a single team.
- * @param team The team to search in.
- * @param id The target player ID.
- * @return player_t* if found, NULL otherwise.
-*/
-static player_t *find_in_team(const teams_t *team, unsigned int id)
-{
-    player_t *pl;
-
-    pl = team->player;
-    while (pl) {
-        if (pl->id == id)
-            return pl;
-        pl = pl->next;
-    }
-    return NULL;
-}
-
-/**
- * @brief Searches for a player by ID across all teams.
- * @param server The server structure containing teams.
- * @param id The target player ID.
- * @return player_t* if found, NULL otherwise.
-*/
-static player_t *find_player_by_id(const server_t *server, unsigned int id)
-{
-    teams_t *team;
-    player_t *pl;
-
-    team = server->teams;
-    pl = NULL;
-    while (team && !pl) {
-        pl = find_in_team(team, id);
-        team = team->next;
-    }
-    return pl;
-}
-
 void cmd_plv(server_t *server, int index, char **args)
 {
     int fd;
