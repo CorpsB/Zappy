ae7f6f8962d1e8ce758a372fa8d2544ffa245c1c
Jason KOENIG <jason.koenig@epitech.eu>
2025-06-20 13:14:57 +0200

    Add: Item finding in look string

---
+++ ai/entity/bot/helper/find_item.cpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2025
+** B-YEP-400-STG-4-1-zappy-noe.carabin
+** File description:
+** find_item
+*/
+
+#include "../../AI.hpp"
+#include "../../../utils/utils.hpp"
+#include <algorithm>
+
+int ai::entity::AI::findItemInLook(const std::string &look, const std::string &item)
+{
+    ai::utils::debug::Logger &logger = ai::utils::debug::Logger::GetInstance();
+
+    if (look.empty() || look.front() != '[' || look.back() != ']') {
+        logger.log("[Error] Bad Look string format: '" + look + "'");
+        return -1;
+    }
+
+    try {
+        std::string inner = look.substr(1, look.size() - 2);
+        std::vector<std::string> tiles = ai::utils::string::split(inner, ',');
+
+        for (int i = 0; i < static_cast<int>(tiles.size()); ++i) {
+            std::vector<std::string> contents = ai::utils::string::split(tiles[i], ' ');
+            if (std::find(contents.begin(), contents.end(), item) != contents.end())
+                return i;
+        }
+        return 0;
+    } catch (const std::exception &e) {
+        logger.log("[Error] Failed parsing Look string '" + look + "': " + e.what());
+        return -1;
+    }
+}
