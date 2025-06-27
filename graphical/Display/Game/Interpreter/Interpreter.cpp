04045d9d4a953d4c62ede3f9149cd3184c9c88ea
Brice LAURENT <brice.laurent@epitech.eu>
2025-06-27 10:58:02 +0200

    Fix: wrong condition for invalid resource number

--- graphical/Display/Game/Interpreter/Interpreter.cpp
+++ graphical/Display/Game/Interpreter/Interpreter.cpp
@@ -458,7 +458,7 @@ void Interpreter::_pdr(const std::smatch &m)
 
     static constexpr std::array<std::string_view, 7> resourcesName = { "FOOD", "LINEMATE", "DERAUMERE", "SIBUR", "MENDIANE", "PHIRAS", "THYSTAME" };
 
-    if (resourceNumber < 1 || resourceNumber > static_cast<int>(resourcesName.size())) {
+    if (resourceNumber > static_cast<int>(resourcesName.size())) {
         std::cerr << "Invalid resource number: " << resourceNumber << std::endl;
         return;
     }
