3ad745c2d44b0f6e0627b97f8583eb72d371d357
ncarabin <noe.carabin@epitech.eu>
2025-06-28 23:23:21 +0200

    Add: Send str func

---
+++ server/src/utils/send_and_recieve/send_str.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappy
+** File description:
+** send_str
+*/
+
+#include "include/include.h"
+#include "include/function.h"
+#include "include/structure.h"
+
+void send_str(server_t *server, int fd, char *message)
+{
+    int lenght = strlen(message);
+    int tmp = 0;
+
+    for (int i = 0; i < lenght ;) {
+        tmp = write(fd, message + i, lenght - i);
+        if (tmp < 0)
+            logger(server, "SEND : WRITE", PERROR, true);
+        i += tmp;
+    }
+}
